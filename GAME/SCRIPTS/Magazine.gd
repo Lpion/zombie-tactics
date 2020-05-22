68f94c36258584ff5f70b1906f3c95e769a90254
Patrick Klostermann <dev@lpion.com>
2020-05-22 13:19:27 +0200

    CHECK FOR GROUP INSTEAD OF METHOD
    - check if entered body is in group("Player")

--- GAME/SCRIPTS/Magazine.gd
+++ GAME/SCRIPTS/Magazine.gd
@@ -6,7 +6,7 @@ func _ready():
 # signal to self
 # check if a body entered the Area and if it has a method
 func body_enter(body: Node) -> void:
-	if body.has_method("pickup_magazine"):
+	if body.is_in_group("Player"):
 		body.pickup_magazine()
 		# remove mag from floor only if player can pick it up
 		if body.CURRENT_MAGS < body.MAX_MAGS:
